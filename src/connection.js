040be19ee4de796d56e7fc42b9e1293050d0d229
Sebastian Restrepo <sebasrestrepom@gmail.com>
2021-08-12 14:52:02 -0500

    test ci

--- src/connection.js
+++ src/connection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: localhost,
+  host: '172.18.0.1',
   user: patientsuser,
   password: patients,
   database: patientsdb,
